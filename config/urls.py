955349c5574c046f44063530bb9c1ff2eda24794
Felipe Holanda <flp9001@gmail.com>
2017-06-23 18:00:53 -0300

    Home page changed to restaurant list

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    url(r'^$', RedirectView.as_view(pattern_name='restaurants:list', permanent=False), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
     # Django Admin, use {% url 'admin:index' %}
